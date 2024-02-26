5017d982f5abb470c6fe8bacf73c01c6b40ca360
Dawid Domiński <dawid.dominski1125@gmail.com>
2024-02-26 11:54:28 +0100

    feat: change home page AddBoard text to white

--- apps/front/src/app/addBoard.tsx
+++ apps/front/src/app/addBoard.tsx
@@ -7,7 +7,7 @@ const AddBoard = () => {
     const { handleOpen, open, handleClose } = useToggleOpen()
 
     return (
-        <div className='flex justify-center lg:justify-start items-center gap-2 text-sm font-medium'>
+        <div className='flex justify-center lg:justify-start items-center gap-2 text-sm font-medium text-white'>
             <button
                 className='w-36 h-8 hover:bg-blue-500 bg-blue-600 transition-all duration-500 rounded flex items-center justify-center'
                 onClick={handleOpen}
