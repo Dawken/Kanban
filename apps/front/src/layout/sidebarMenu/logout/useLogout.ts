49912fc1c0561d2a9eca681a323ac4f10038f7e2
Dawid Domiński <dawid.dominski1125@gmail.com>
2024-03-07 19:40:15 +0100

    feat: disable user ability to use backbutton after logout

--- apps/front/src/layout/sidebarMenu/logout/useLogout.ts
+++ apps/front/src/layout/sidebarMenu/logout/useLogout.ts
@@ -1,14 +1,11 @@
 import { useMutation } from '@apollo/client'
 import { LOGOUT_USER } from '@src/graphQL/auth/mutations'
 import { toast } from 'react-toastify'
-import { useRouter } from 'next/navigation'
 
 const useLogout = () => {
-    const router = useRouter()
-
     const [logoutUser, { loading }] = useMutation(LOGOUT_USER, {
         onCompleted: () => {
-            router.push('/login')
+            window.location.href = '/login'
             toast.success('Your session has expired')
         },
         onError: () => {
